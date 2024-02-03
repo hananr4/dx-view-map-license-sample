051d9a80cb0a72f889a1e99a7321f03bea065b12
Hanan Rodriguez <hanan.rodriguez@greensoft.com.ec>
2024-02-02 21:17:43 -0500

    License

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+// // import { DxButtonModule } from 'devextreme-angular';
+//  import DevExpress from 'devextreme';
+//  import { licenseKey } from '../devextreme-key';
+
+// DevExpress.config({
+//   licenseKey: licenseKey
+// });
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'view-map';
+  AppComponent() {
+
+    
+
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
+import DevExpress from 'devextreme';
+import { licenseKey } from '../devextreme-key';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), { provide: 'AppTitle', useValue: 'My App' } 
+  ]
 };
+
+
+// DevExpress.config({licenseKey: licenseKey})
